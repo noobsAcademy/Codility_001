88369b9fdb0b0a0976e3d03ac8eb83005eceead4
Daniel Otero <danibueu@gmail.com>
2018-08-20 20:02:22 +0200

    Inicializando

---
+++ Solution.java
@@ -0,0 +1,14 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+package binarygap;
+class Solution {
+    //Must return the max number of consecutive 0s between 1s
+    //Example: N = 41 -> Binary = 101001 -> Máx = 2
+    //Example: N = 40 -> Binary = 101000 -> Máx = 1
+    public static int solution(int N) {
+
+    }
+}
