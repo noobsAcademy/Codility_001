e24a28066caa4b051f4849645e58e1119cf6109f
Daniel Otero <danibueu@gmail.com>
2018-08-20 20:05:14 +0200

    Dani_Solución_Matemática

--- Solution.java
+++ Solution.java
@@ -9,6 +9,21 @@ class Solution {
     //Example: N = 41 -> Binary = 101001 -> Máx = 2
     //Example: N = 40 -> Binary = 101000 -> Máx = 1
     public static int solution(int N) {
-
+        int maxCeros=0;
+        int actCeros=0;
+        while (N>1) {
+            if  (N%2==0)  {
+                N/=2;
+                continue;
+            }
+            N/=2;
+            while (N%2==0) {
+                actCeros++;
+                N/=2;
+            }
+            if (maxCeros<actCeros) maxCeros = actCeros;
+            actCeros=0;
+        }
+        return maxCeros;
     }
 }
