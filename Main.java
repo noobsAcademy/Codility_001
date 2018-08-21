c600d3b86599a85a19eae67be25d15fc110be099
droidsam1 <droidsam@gmail.com>
2018-08-21 09:24:37 +0200

    delete old files

--- Main.java
+++
@@ -1,7 +0,0 @@
-package binarygap;
-
-public class Main {
-    public void main (String[] args){
-        System.out.println("Mayor número de espacios: "+Solution.solution(Integer.parseInt(args[1])));
-    }
-}
