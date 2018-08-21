253d6df46f3008a262cc8e9febeb8675c6e19308
droidsam1 <droidsam@gmail.com>
2018-08-21 15:07:09 +0200

    solucion sam

--- binaryGap/src/main/java/noobsAcademy/binaryGap/Solution.java
+++ binaryGap/src/main/java/noobsAcademy/binaryGap/Solution.java
@@ -11,6 +11,24 @@ public class Solution {
     // Example: N = 40 -> Binary = 101000 -> Máx = 1
     public static int solution(int N) {
 	// TODO implementar tu código aqui
-	throw new UnsupportedOperationException("Not Implemented yet");
+	String binaryString = Integer.toBinaryString(N);
+	int maxNumberOfConsecutiveZeros = 0;
+	int zerosCounter = 0;
+	for (char c : binaryString.toCharArray()) {
+	    if (c == '0') {
+		zerosCounter++;
+	    } else {
+		maxNumberOfConsecutiveZeros = getMax(zerosCounter, maxNumberOfConsecutiveZeros);
+		zerosCounter = 0;
+	    }
+	}
+	return maxNumberOfConsecutiveZeros;
+    }
+
+    private static int getMax(int zerosCounter, int maxNumberOfConsecutiveZeros) {
+	if (zerosCounter > maxNumberOfConsecutiveZeros) {
+	    return zerosCounter;
+	}
+	return maxNumberOfConsecutiveZeros;
     }
 }
